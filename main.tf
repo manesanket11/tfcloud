75b9609dc4634a474fa1f500df8f795065a79383
Sanket Mane <sanketmaneofficial@gmail.com>
2022-09-11 02:42:53 +0530

    changing instance type

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-      access_key = "AKIAWGUFV5YD3RBYPFDG"
+ access_key = "AKIAWGUFV5YD3RBYPFDG"
   secret_key = "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"
   region  = "us-east-1"
 }
 resource "aws_instance" "web" {
   ami           ="ami-039a49e70ea773ffc"
-  instance_type = "t2.micro"
+  instance_type = "t2.nano"
 
   tags = {
     Name = "HelloWorld"
