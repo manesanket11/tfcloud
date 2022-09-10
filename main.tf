f650f146b1247f2a229fc584d99e9af544df0a8c
Sanket Mane <60425907+manesanket11@users.noreply.github.com>
2022-09-11 02:31:37 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  version = "~> 2.0"
+  region  = "us-east-1"
+}
+resource "aws_instance" "web" {
+  ami           ="ami-039a49e70ea773ffc"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
