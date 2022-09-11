d5570bb4e5b5c6c438e3b05d91dddb221708388d
Sanket Mane <sanketmaneofficial@gmail.com>
2022-09-11 16:41:32 +0530

    script fix

--- main.tf
+++ main.tf
@@ -1,15 +1,13 @@
 
-/*   module "ec2_test" {
-  module "ec2_test" {
+module "ec2_test" {
   source = "./Modules"
 
   instance_type = "t2.micro"
-  name        = "test"
+  name          = "test"
 }
 
 output "ec2" {
-  value       = module.ec2_test.*
+  value = module.ec2_test.*
 
-} 
+}
 
-  */
