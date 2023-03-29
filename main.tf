e4d95a5e8990c7e5655167efc08848f8068150f3
Sanket Mane <60425907+manesanket11@users.noreply.github.com>
2023-03-29 14:53:02 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,11 @@
 
 module "ec2_test" {
-  module "ec2_test" {
   source = "./Modules"
 
   instance_type = "t2.micro"
   name        = "test"
 }
-    }
+  
 output "ec2" {
   value       = module.ec2_test.*
 
